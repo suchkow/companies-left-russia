dbbb2b17bba89aba756557eb676a8fc8ee168222
suchkow <makeeoutchill@gmail.com>
2023-05-10 23:58:37 +0300

    Added & updated files

--- src/test-clustering.py
+++ src/test-clustering.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 n_samples = 4000
 n_components = 4
 
-X, y_true = make_blobs(
-    n_samples=n_samples, centers=n_components, cluster_std=0.60, random_state=0
-)
+X, y_true = make_blobs(n_samples=n_samples, centers=n_components, cluster_std=0.60, random_state=0)
 X = X[:, ::-1]
 
 # Calculate seeds from k-means++
