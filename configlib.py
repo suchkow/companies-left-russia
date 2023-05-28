9d424b5792e0ae2749507b1303dac20a6bb6e4f8
suchkow <makeeoutchill@gmail.com>
2023-05-28 18:54:54 +0300

    Added & updated files

--- configlib.py
+++ configlib.py
@@ -1,4 +1,6 @@
 import json
+import pandas as pd
+import os
 import config as cf
 
 
@@ -40,19 +42,31 @@ def get_country_ru_eng() -> object:
 
 
 # --------------- FUNCS-HELPERS --------------- #
-def save_new_df(df_name: str, data_frame: object) -> int:
-    # LIST_DF.append(data_frame.copy(deep=True))
-    cf.DICT_DF[df_name] = data_frame.copy(deep=True)
-    return len(cf.DICT_DF)
+# def save_new_df(df_name: str, data_frame: object):
+#     cf.DICT_DF[df_name] = data_frame.copy(deep=True)
+#     print('New DataFrame saved!\n\n' + 'Name: ' + df_name)
+#     print(cf.DICT_DF[df_name].info(verbose=False))
+#     return
+
+def save_new_df(file_name: str, data_frame: object):
+    path = f'../data/pickles/{file_name}.pkl'
+    data_frame.to_pickle(path)
+
+    print('New DataFrame saved!\n\n' + 'File path: ' + path)
+    print(pd.read_pickle(path).info(verbose=False))
+    return
 
 
-def call_df(df_name: str) -> object:
-    return cf.DICT_DF[df_name]
+def call_df(file_name: str) -> object:
+    path = f'../data/pickles/{file_name}.pkl'
+    return pd.read_pickle(path)
 
 
 def list_saved():
-    if bool(cf.DICT_DF):
-        print(json.dumps(cf.DICT_DF, indent=4, sort_keys=True))
+    if len(os.listdir('../data/pickles/')) != 0:
+        pkl_ls = [f for f in os.listdir('../data/pickles/') if os.path.isfile(os.path.join('../data/pickles/', f))]
+        print('All the DataFrames saved under ../data/pickles/:')
+        print(*pkl_ls, sep="\n")
     else:
-        print('DICT_DF is empty.')
+        print('There is no new DataFrames saved.')
     return
